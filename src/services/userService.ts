29914e22131b815e98ddab4591009c545f841a29
nqui43-len <nqui43@gmail.com>
2026-09-12 15:03:12 +0700

    sua loi cap nhat ho so nguoi dung

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,6 +3,6 @@ import axiosClient from './axiosClient';
 
 export const userService = {
   updateProfile: async (data: { username: string; avatarUrl: string }) => {
-    return axiosClient.patch('/users/profile', data);
+    return axiosClient.patch('/auth/profile', data);
   }
 };
